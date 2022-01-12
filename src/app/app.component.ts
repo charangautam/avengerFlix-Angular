2a30719170a459e008fbf10a6ddb968233290678
Charan Gautam <gautamsaicharan@yahoo.com>
2022-01-11 18:26:54 -0700

    user registration form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { MatDialog } from '@angular/material/dialog';
+
+// embedded components
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'avengersFlix-Angular';
+
+  constructor(public dialog: MatDialog) { }
+
+  // function that will open the dialog when the register button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '480px'
+    })
+  }
 }
---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// import to close dialog on success 
+import { MatDialogRef } from '@angular/material/dialog';
+// import API calls from backend 
+import { UserRegistrationService } from '../fetch-api-data.service';
+// import to display notification back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' }
+
+  /**
+   * * Called when creating an instance of the class
+   * @param fetchApiData
+   * @param dialogRef
+   * @param snackBar
+  */
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  /**
+   * Function for sending the form inputs to the backend to create a new user
+   * @returns alert indicating a successful registration or an error
+  */
+
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      // logic for successful user registration
+      console.log(result)
+      this.dialogRef.close() // close modal on success 
+      this.snackBar.open('User registration successful', 'OK', {
+        duration: 2000
+      })
+    }, (result) => {
+      console.log(result);
+      this.snackBar.open("Username already exists. Please try a different username", 'OK', {
+        duration: 2000
+      });
+    })
+  }
+
+
+}
